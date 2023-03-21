157b3e2093947d1c43ff6c290042fe33c67b1273
seperool <seperool@gmail.com>
2023-03-21 10:32:00 -0300

    biblitecas py

---
+++ bibliotecas/bibliotecas_python.py
@@ -0,0 +1,22 @@
+# Sistema operacional
+import os
+
+# Dados
+import math
+import numpy as np
+import pandas as pd
+
+# Machine learn
+import tensorflow as tf #tensorflow
+from tensorflow import keras #keras
+from tensorflow.keras import layers
+
+# Tratamento de dados
+import sklearn #scikit-learn
+
+# Visulização de dados
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Interface grafíca
+import kivy
